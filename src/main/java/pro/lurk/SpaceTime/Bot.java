56d5c4a8265c8c81cc99c02829f669a3964319b6
binaryconjurer <techboylabs@gmail.com>
2018-10-20 20:08:56 -0700

    Updated JDA to new .build and .awaitReady method vs deprecated method. Among cleaning up with direct access to JDA object with getAPI

--- src/main/java/pro/lurk/SpaceTime/Bot.java
+++ src/main/java/pro/lurk/SpaceTime/Bot.java
@@ -1,22 +1,48 @@
 package pro.lurk.SpaceTime;
 
+import java.io.IOException;
+
+import javax.security.auth.login.LoginException;
+
 import net.dv8tion.jda.core.AccountType;
 import net.dv8tion.jda.core.JDA;
 import net.dv8tion.jda.core.JDABuilder;
 import net.dv8tion.jda.core.entities.Game;
+import net.dv8tion.jda.core.events.Event;
+import net.dv8tion.jda.core.events.ReadyEvent;
+import net.dv8tion.jda.core.hooks.EventListener;
 
 public class Bot {
+	private static JDA api;
+
+	public static JDA getAPI() {
+		return api;
+	}
+
 	public static void main(String[] arguments) throws Exception {
-		ConfigLoader c = new ConfigLoader();
 
-		JDA api = new JDABuilder(AccountType.BOT).setToken(c.getToken()).setGame(Game.playing("Time Travel"))
-				.buildAsync();
+		setupBot();
+	}
 
-		// Registers MyListern Event
-		// Contains commands and custom logic for the bot
-		api.addEventListener(new MyListener());
-
-		//System.out.println("Working Directory = " + System.getProperty("user.dir"));
+	// Setups the bot by retriving config data, logging in, setting the game, and adding listener(s) for functionality.
+	private static void setupBot() throws IOException {
+		ConfigLoader config;
+		try {
+			config = new ConfigLoader();
+			JDABuilder jdaBuilder = new JDABuilder(AccountType.BOT).setToken(config.getToken());
+			// Set initial game playing
+			jdaBuilder.setGame(Game.playing("Time Travel!"));
+			// Setup Event Listeners
+			jdaBuilder.addEventListener(new Calculator());
+			// Finally build
+			api = jdaBuilder.build();
+			api.awaitReady();
+			// TODO Actually handle the expectations
+		} catch (LoginException e) {
+			e.printStackTrace();
+		} catch (InterruptedException e) {
+			e.printStackTrace();
+		}
 
 	}
 }
