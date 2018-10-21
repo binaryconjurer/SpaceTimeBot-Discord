475e8e0db07720564322c499a250644f139cd27d
BuildTools <unconfigured@null.spigotmc.org>
2018-10-20 20:13:22 -0700

    Reanmed MyListener to Calculator to reflect on it's function better. Wizard101 Pet Calculator.

--- src/main/java/pro/lurk/SpaceTime/Calculator.java
+++ src/main/java/pro/lurk/SpaceTime/Calculator.java
@@ -11,7 +11,7 @@ import net.dv8tion.jda.core.entities.MessageChannel;
 import net.dv8tion.jda.core.events.message.MessageReceivedEvent;
 import net.dv8tion.jda.core.hooks.ListenerAdapter;
 
-public class MyListener extends ListenerAdapter {
+public class Calculator extends ListenerAdapter {
 	// Command prefix
 	public static final String prefix = "!";
 	public static int prefixL = prefix.length();
