a34a6541174138d4c29d1607748c1ea1bb640175
binaryconjurer <techboylabs@gmail.com>
2018-11-12 19:55:05 -0800

    Expanded EmbedManger and Helper with Footer

--- src/main/java/pro/lurk/command/EmbedHelper.java
+++ src/main/java/pro/lurk/command/EmbedHelper.java
@@ -3,17 +3,46 @@ package pro.lurk.command;
 import java.util.HashMap;
 
 public class EmbedHelper {
-	private String authorName = "Thetechman";
+
+	private String authorName = "";
 	private String authorURL = "";
 	private String authorIconURL = "";
 	private String title = "";
 	private String titleURL = "";
-	private String description = "This is a description!";
-	private String color = "d96017";
+	private String description = "";
+	private int color = 0xd96017;
 	private String image = "";
 	private String thumbnail = "";
 	private HashMap<String, String> fields = new HashMap<String, String>();
+	private String footer = "";
+	private String footerURL = "";
+
+	public String getFooterURL() {
+		return footerURL;
+	}
+
+	public void setFooterURL(String footerURL) {
+		this.footerURL = footerURL;
+	}
+
+	public String getFooter() {
+		return footer;
+	}
+
+	public void setFooter(String footer) {
+		this.footer = footer;
+	}
+
 	private long messageID = 0;
+
+	@Override
+	public String toString() {
+		return "EmbedHelper [getMessageID()=" + getMessageID() + ", getAuthorURL()=" + getAuthorURL()
+				+ ", getAuthorIconURL()=" + getAuthorIconURL() + ", getTitle()=" + getTitle() + ", getTitleURL()="
+				+ getTitleURL() + ", getDescription()=" + getDescription() + ", getColor()=" + getColor()
+				+ ", getImage()=" + getImage() + ", getThumbnail()=" + getThumbnail() + ", getFields()=" + getFields()
+				+ ", getAuthorName()=" + getAuthorName() + "]";
+	}
 
 	public long getMessageID() {
 		return messageID;
@@ -67,11 +96,11 @@ public class EmbedHelper {
 		this.description = description;
 	}
 
-	public String getColor() {
+	public int getColor() {
 		return color;
 	}
 
-	public void setColor(String color) {
+	public void setColor(int color) {
 		this.color = color;
 	}
 
--- src/main/java/pro/lurk/command/EmbedManager.java
+++ src/main/java/pro/lurk/command/EmbedManager.java
@@ -2,7 +2,9 @@ package pro.lurk.command;
 
 import java.awt.Color;
 import java.util.Arrays;
+import java.util.HashMap;
 import java.util.List;
+import java.util.Map.Entry;
 
 import net.dv8tion.jda.core.EmbedBuilder;
 import net.dv8tion.jda.core.entities.Message;
@@ -33,34 +35,115 @@ public class EmbedManager extends Command {
 			// TODO Add better help message
 			channel.sendMessage(author.getAsMention()
 					+ ", Please add an embed with at least a title, and a description if you like!").queue();
-			;
+
 		}
 		// ADD
-		// .embed add [title]
+		// .embed add -t [title]
 		else if (args[1].equalsIgnoreCase("add") && args[1].equals("add") && args.length == 3) {
 			EmbedHelper helper = new EmbedHelper();
 			helper.setTitle(getDescription(args, message));
-			channel.sendMessage(minnalEmbed(helper).build()).queue();
+			channel.sendMessage(defaultEmbed(helper).build()).queue();
 			helper.setMessageID(getLastMessageIDByUser(channel, Bot.getAPI().getSelfUser()));
 			db.save(helper);
 
 		}
-		// .embed add [title] [description]
+		// .embed add -t [title] -d [description]
 		else if (args[1].equalsIgnoreCase("add") && args.length >= 4) {
 			EmbedHelper helper = new EmbedHelper();
 			helper.setTitle(getDescription(args, message));
 			helper.setDescription(getDescription(args, message));
-			channel.sendMessage(minnalEmbed(helper).build()).queue();
+			channel.sendMessage(defaultEmbed(helper).build()).queue();
 			helper.setMessageID(getLastMessageIDByUser(channel, Bot.getAPI().getSelfUser()));
 			db.save(helper);
 		}
 		// .embed edit [title]
 		else if (args[1].equalsIgnoreCase("add") && args.length == 2) {
-			
+
 		}
 
 	}
-	
+
+	private EmbedBuilder defaultEmbed(EmbedHelper helper) {
+		EmbedBuilder embed = new EmbedBuilder();
+		embed.setAuthor(helper.getAuthorName());
+		embed.setTitle(helper.getTitle());
+		embed.setDescription(helper.getDescription());
+		embed.setColor(new Color(helper.getColor()));
+		embed.addField("Field Title", "This is text!", false);
+		return embed;
+	}
+
+	private EmbedBuilder customEmbed(EmbedHelper helper) {
+		EmbedBuilder customEmbed = new EmbedBuilder();
+		// Just AuthorName
+
+		
+
+		customEmbed.setColor(new Color(helper.getColor()));
+
+		// Just Author
+		if (!helper.getAuthorName().isEmpty() && helper.getAuthorURL().isEmpty()) {
+			customEmbed.setAuthor(helper.getAuthorName());
+		}
+		// AuthorName and AuthorURL
+		if (!helper.getAuthorName().isEmpty() && !helper.getAuthorURL().isEmpty()) {
+			customEmbed.setAuthor(helper.getAuthorName(), helper.getAuthorURL());
+		}
+		// AuthorName and AuthorIconURL
+		if (!helper.getAuthorName().isEmpty() && helper.getAuthorURL().isEmpty()
+				&& !helper.getAuthorIconURL().isEmpty()) {
+			customEmbed.setAuthor(helper.getAuthorName(), null, helper.getAuthorIconURL());
+		}
+		// AuthorName, AuthorURL, and AuthorIconURL
+		if (!helper.getAuthorName().isEmpty() && !helper.getAuthorURL().isEmpty()
+				&& !helper.getAuthorIconURL().isEmpty()) {
+			customEmbed.setAuthor(helper.getAuthorName(), helper.getAuthorURL(), helper.getAuthorIconURL());
+		}
+		// Just Title
+		if (!helper.getTitle().isEmpty() && helper.getTitleURL().isEmpty()) {
+			customEmbed.setTitle(helper.getTitle());
+		}
+		// Title and TitleURL
+		if (!helper.getTitle().isEmpty() && !helper.getTitleURL().isEmpty()) {
+			customEmbed.setTitle(helper.getTitle(), helper.getTitleURL());
+		}
+		// Description
+		if (!helper.getDescription().isEmpty()) {
+			customEmbed.setDescription(helper.getDescription());
+		}
+		
+		// Image
+		if (!helper.getImage().isEmpty()) {
+			customEmbed.setImage(helper.getImage());
+		}
+		// Thumbnail
+		if (!helper.getThumbnail().isEmpty()) {
+			customEmbed.setThumbnail(helper.getThumbnail());
+		}
+		// Just Footer
+		if (!helper.getFooter().isEmpty() && helper.getFooterURL().isEmpty()) {
+			customEmbed.setFooter(helper.getFooter(), null);
+		}
+		// Footer and Footer URL
+		if (!helper.getFooter().isEmpty() && !helper.getFooterURL().isEmpty()) {
+			customEmbed.setFooter(helper.getFooter(), helper.getFooterURL());
+		}
+
+		// Fields
+		HashMap<String, String> fields = new HashMap<String, String>();
+		fields = helper.getFields();
+
+		// Fields
+		for(Entry<String, String> e : fields.entrySet()) {
+	        String key = e.getKey();
+	        String value = e.getValue();
+	        
+	        customEmbed.addField(key, value, false);
+	        
+	    }
+		return customEmbed;
+	}
+
 	private String getTitle(String args[], Message message) {
 
 		int messageLocation = args[0].length() + 1 + args[1].length() + 1;
@@ -73,16 +156,6 @@ public class EmbedManager extends Command {
 		int messageLocation = args[0].length() + 1 + args[1].length() + 1 + args[2].length() + 1;
 		String fullDescription = message.getContentDisplay().substring(messageLocation);
 		return fullDescription;
-	}
-
-	private EmbedBuilder minnalEmbed(EmbedHelper helper) {
-		EmbedBuilder embed = new EmbedBuilder();
-		embed.setAuthor(helper.getAuthorName());
-		embed.setTitle(helper.getTitle());
-		embed.setDescription(helper.getDescription());
-		embed.setColor(new Color(Integer.parseInt(helper.getColor(), 16)));
-		embed.addField("Field Title", "This is text!", false);
-		return embed;
 	}
 
 	private long getLastMessageIDByUser(MessageChannel channel, User user) {
